2324afaf50d622f7a2a012fabff050c797134409
odindoni <dion3407@gmail.com>
2025-04-20 15:54:29 +0800

    Initial commit: Prompted Vite version

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-4xl font-bold text-blue-800">Prompted (Vite + Tailwind)</h1>
+    </div>
+  );
+}
+
+export default App;
